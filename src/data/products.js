f8b67ebc0c18b0784f3f9ad256589e06c427c7de
gramlesslocation-svg <gramlesslocation@gmail.com>
2026-07-05 17:13:40 +0300

    Create src/data/products.js

---
+++ src/data/products.js
@@ -0,0 +1,13 @@
+export const products = [
+  { id: "1", name: "Rice Golden", size: "800g", price: 5 },
+  { id: "2", name: "Rice Golden", size: "2.4kg", price: 12 },
+
+  { id: "3", name: "Rice Steam", size: "800g", price: 5 },
+  { id: "4", name: "Rice Steam", size: "2.4kg", price: 12 },
+
+  { id: "5", name: "Rice Creamy", size: "800g", price: 5 },
+  { id: "6", name: "Rice Creamy", size: "2.4kg", price: 12 },
+
+  { id: "7", name: "Rice White", size: "800g", price: 5 },
+  { id: "8", name: "Rice White", size: "2.4kg", price: 12 },
+];
